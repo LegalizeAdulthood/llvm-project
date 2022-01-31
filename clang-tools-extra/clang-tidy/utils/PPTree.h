71a6c7d6fb0ce29615f2ad88797a8869decc1314
Richard <legalize@xmission.com>
2022-01-31 16:02:46 -0700

    Stub out PPTree files

---
+++ clang-tools-extra/clang-tidy/utils/PPTree.h
@@ -0,0 +1,12 @@
+//===---------- PPTree.h - clang-tidy -------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_PPTREE_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_PPTREE_H
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_PPTREE_H
